20530dc5e2552ca12869bb5f17285763882537cf
bbenny95 <Bbenny6347@conestogac.on.ca>
2022-08-18 02:52:05 -0400

    test

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/app-ui'));
+app.use(express.static(__dirname + '/dist/app_ui'));
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/app-ui/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/app_ui/index.html'));
 });
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8080);
